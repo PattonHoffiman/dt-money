16892b85d3ed711c43dd6015d0df584af19d2057
Patton Hoffiman <patton.hoffiman@navegarti.com.br>
2021-10-18 18:25:06 -0300

    [add] Create Summary component

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  
+  gap: 2rem;
+  grid-template-columns: repeat(3, 1fr);
+
+  margin-top: -10rem;
+
+  div {
+    color: var(--text-title);
+    background: var(--shape);
+    
+    padding: 1.5rem 2rem;
+    border-radius: .25rem;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong {
+      display: block;
+      margin-top: 1rem;
+
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem;
+    }
+
+    &.hightlight {
+      color: white;
+      background: var(--green);
+    }
+  }
+`;
