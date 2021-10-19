03e22c3e1c0d4ac76f875cc879c215cf6970cfed
Patton Hoffiman <patton.hoffiman@navegarti.com.br>
2021-10-19 16:45:10 -0300

    [upd] Create modal styles and finish modal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
+
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -9,12 +16,56 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 } : NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+
+      className="modal-content"
+      overlayClassName="modal-overlay"
     >
-      <h2>Cadastrar transação</h2>
+      <button
+        type="button"
+        className="modal-close"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            activeColor='green'
+            isActive={type === 'deposit'}
+            onClick={() => {setType('deposit')}}
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            activeColor='red'
+            isActive={type === 'withdraw'}
+            onClick={() => {setType('withdraw')}}
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,107 @@
 import styled from 'styled-components';
+import { darken, transparentize } from 'polished';
 
-export const Container = styled.div``;
+const colors = {
+  red: '#e52e4d',
+  green: '#33cc95'
+};
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+export const Container = styled.form`
+  h2 {
+    font-size: 1.5rem;
+    color: var(--text-title);
+
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    height: 4rem;
+
+    padding: 0 1.5rem;
+
+    background: #e7e9ee;
+    border-radius: .25rem;
+    border: 1px solid #d7d7d7;
+
+    font-size: 1rem;
+    font-weight: 400;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type='submit'] {
+    width: 100%;
+    height: 4rem;
+
+    font-size: 1rem;
+    font-weight: 600;
+
+    padding: 0 1.5rem;
+    margin-top: 1.5rem;
+
+    border: 0;
+    color: white;
+    border-radius: .25rem;
+    background: var(--green);
+
+    transition: filter .2s;
+
+    &:hover {
+      filter: brightness(.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  
+  display: grid;
+  
+  gap: .5rem;
+  grid-template-columns:  1fr 1fr;
+`;
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border-radius: .25rem;  
+  border: 1px solid #d7d7d7;
+  background: ${(props) => props.isActive
+    ? transparentize(.9, colors[props.activeColor])
+    : 'transparent'
+  };
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color .2s;
+
+  &:hover {
+    border-color: ${darken(.1, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+
+    font-size: 1rem;
+    margin-left: 1rem;
+    color: var(--text-title);
+  }
+`;
